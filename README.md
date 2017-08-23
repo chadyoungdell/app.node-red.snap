13e9266f94c49e5ae22355966dc64814859a2672
chadyoungdell.com <chad.young@dell.com>
2017-08-23 12:46:21 -0500

    initial upload of forked repo

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# nodered.snap
-Node  RED snappy package
+# Node-Red for Ubuntu Core
+The original code for this came from https://github.com/muka/nodered.snap.git.  
+I have since changes a few things to make it work for me.  
+To build, just clone the repository and run `snapcraft` in it 
+  
+    Listens on port 1880
+    Logging is set to:
+    level = debug  
+    metric = true  
+    audit = true
 
-To build, just clone the repository and run `snapcraft` in it
-
-Listens on port 1880
--- settings/settings.js
+++ settings/settings.js
@@ -173,12 +173,12 @@ module.exports = {
             // info - record information about the general running of the application + warn + error + fatal errors
             // debug - record information which is more verbose than info + info + warn + error + fatal errors
             // trace - record very detailed logging + debug + info + warn + error + fatal errors
-            level: "info",
+            level: "debug",
 
             // Whether or not to include metric events in the log output
-            metrics: false,
+            metrics: true,
             // Whether or not to include audit events in the log output
-            audit: false
+            audit: true
         }
     }
 }
